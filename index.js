08b5cb68331fc0503e534d995cf689a7e97e9625
Spencer von der Ohe <s.vonderohe40@gmail.com>
2020-06-28 12:22:38 -0600

    Add shellbang to fix cli issue

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+#!/usr/bin/env node
+
 const fs = require('fs');
 const toml = require('toml-patch');
 const yaml = require('yaml-boost');
@@ -10,9 +12,11 @@ yaml-to-toml <yaml_path> <toml_path>
 if (args.length < 2){
     console.log('Too few arguments');
     console.log(expected);
+    process.exit(1);
 } else if(args.length > 2){
     console.log('Too many arguments');
     console.log(expected);
+    process.exit(1);
 }
 
 const parsedData = yaml.load(args[0]);
